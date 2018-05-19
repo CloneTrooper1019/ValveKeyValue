f02d7bc373e715be06385535d33e18cc74f8c290
Pavel Djundik <github@xpaw.me>
2018-05-19 12:56:31 +0300

    Add test for bracketed value that currently fails

---
+++ ValveKeyValue/ValveKeyValue.Test/Text/BracketsWithoutQuotesTestCase.cs
@@ -0,0 +1,27 @@
+﻿using System.IO;
+using System.Linq;
+using NUnit.Framework;
+
+namespace ValveKeyValue.Test
+{
+    class BracketsWithoutQuotesTestCase
+    {
+        [Test]
+        public void CorrectlyAssumesBracketedValueIsAString()
+        {
+            Assert.That((string)data["$envmapsaturation"], Is.EqualTo("[.5 .5 .5]"));
+        }
+
+        KVObject data;
+
+        [OneTimeSetUp]
+        public void SetUp()
+        {
+            var options = new KVSerializerOptions { HasEscapeSequences = true };
+            using (var stream = TestDataHelper.OpenResource("Text.brackets_without_quotes.vdf"))
+            {
+                data = KVSerializer.Create(KVSerializationFormat.KeyValues1Text).Deserialize(stream, options);
+            }
+        }
+    }
+}
